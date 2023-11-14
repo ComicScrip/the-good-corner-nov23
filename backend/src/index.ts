f91295876a635af4e3c7c16089f140ee41a0b337
Pierre Genthon <genthon.pierre@gmail.com>
2023-11-14 13:59:14 +0100

    lc express 1

---
+++ backend/src/index.ts
@@ -0,0 +1,51 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+const ads = [
+  {
+    id: 1,
+    title: "Bike to sell",
+    description:
+      "My bike is blue, working fine. I'm selling it because I've got a new one",
+    owner: "bike.seller@gmail.com",
+    price: 100,
+    picture:
+      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
+    location: "Paris",
+    createdAt: "2023-09-05T10:13:14.755Z",
+  },
+  {
+    id: 2,
+    title: "Car to sell",
+    description:
+      "My car is blue, working fine. I'm selling it because I've got a new one",
+    owner: "car.seller@gmail.com",
+    price: 10000,
+    picture:
+      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
+    location: "Paris",
+    createdAt: "2023-10-05T10:14:15.922Z",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/ads", (req, res) => {
+  res.send(ads);
+});
+
+app.post("/ads", (req, res) => {
+  console.log(req.body);
+  ads.push(req.body);
+  res.send(req.body);
+});
+
+app.listen(port, () => {
+  console.log("server ready");
+});
