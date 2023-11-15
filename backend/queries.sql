99324ffc50c79765a282243c7736cba2f18c2f71
Pierre Genthon <genthon.pierre@gmail.com>
2023-11-15 16:50:55 +0100

    atelier

--- backend/queries.sql
+++ backend/queries.sql
@@ -1,5 +1,3 @@
-DROP table ad;
-
 CREATE TABLE ad 
 (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -9,20 +7,33 @@ CREATE TABLE ad
 	price REAL,
     picture VARCHAR(255),
     location VARCHAR(100),
-	createdAt DATE('NOW')
-	category VARCHAR(100)
+	createdAt DATE,
+	category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-INSERT INTO ad (title, owner, price, location, createdAt) VALUES 
-    ('Vieux jeans troués', 'Nick', 200, 'Bordeaux', '2023-09-01'),
-    ('T-shit hello world', 'Pierre', 9.99, 'Lyon', '2023-09-02'),
-    ('chausettes wild code school', 'Anna', 4.49, 'Paris', '2023-09-01'),
-    ('R5 pour pièces', 'Patrick', 100, 'Bordeaux', '2023-09-10'),
-    ('DeLorean DMC-12', 'Marty', 70000, 'Paris', '2023-09-13'),
-    ('Peugeot 206', 'Amélie', 2000, 'Lyon', '2023-09-05'),
-    ('Opel Corsa', 'Lucie', 1000, 'Bordeaux', '2023-09-06'),
-    ('Jeu de cartes pokémon collector', 'Jean', 350, 'Lyon', '2023-09-06'),
-    ('Chaine hifi complète', 'Jean', 80, 'Paris', '2023-09-01');
+CREATE TABLE category 
+(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name  VARCHAR(100) NOT NULL
+);
+
+INSERT INTO category (name) VALUES 
+    ('vêtement'),
+    ('voiture'),
+    ('autre');
+
+
+INSERT INTO ad (title, owner, price, location, createdAt, category_id) VALUES 
+    ('Vieux jeans troués', 'Nick', 200, 'Bordeaux', '2023-09-01', 1),
+    ('T-shit hello world', 'Pierre', 9.99, 'Lyon', '2023-09-02', 1),
+    ('chausettes wild code school', 'Anna', 4.49, 'Paris', '2023-09-01', 1),
+    ('R5 pour pièces', 'Patrick', 100, 'Bordeaux', '2023-09-10', 2),
+    ('DeLorean DMC-12', 'Marty', 70000, 'Paris', '2023-09-13', 2),
+    ('Peugeot 206', 'Amélie', 2000, 'Lyon', '2023-09-05', 2),
+    ('Opel Corsa', 'Lucie', 1000, 'Bordeaux', '2023-09-06', 2),
+    ('Jeu de cartes pokémon collector', 'Jean', 350, 'Lyon', '2023-09-06', 3),
+    ('Chaine hifi complète', 'Jean', 80, 'Paris', '2023-09-01', 3);
 
 
 SELECT * FROM ad; 
@@ -36,3 +47,11 @@ UPDATE ad SET price = 0 WHERE createdAt = '2023-09-01'
 SELECT AVG(price) FROM ad WHERE location = 'Paris'
 
 SELECT location, AVG(price) as 'prix moyen des annonces' FROM ad GROUP BY location
+
+SELECT * FROM ad WHERE category_id = 1;
+
+SELECT * FROM ad WHERE category_id IN(1, 2);
+
+SELECT AVG(price) FROM ad WHERE category_id = 3;
+
+SELECT * FROM ad as a JOIN category as c ON a.category_id = c.id WHERE c.name LIKE 'v%';
