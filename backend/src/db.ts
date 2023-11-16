01a55c29b6796f6a4733e56ee25e95a2ea98514e
Pierre Genthon <genthon.pierre@gmail.com>
2023-11-16 15:57:10 +0100

    atelier

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export default new DataSource({
   type: "sqlite",
-  database: "src/the_good_corner.sqlite",
+  database: "the_good_corner.sqlite",
   entities: ["src/entities/*.ts"],
   synchronize: true,
 });
