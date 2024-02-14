ff2610140ccebbaf0c17e968ac35a0ae56fe303d
Pierre Genthon <genthon.pierre@gmail.com>
2024-02-14 09:36:52 +0100

    dockerfiles front back prod

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,8 +1,11 @@
 import { DataSource } from "typeorm";
+import Ad from "./entities/Ad";
+import Category from "./entities/Category";
+import Tag from "./entities/Tag";
 
 export default new DataSource({
   type: "sqlite",
   database: "the_good_corner.sqlite",
-  entities: ["src/entities/*.ts"],
+  entities: [Ad, Category, Tag],
   synchronize: true,
 });
