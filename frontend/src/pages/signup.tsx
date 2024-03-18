2f81c8fa34d8c10e63861910bd83dbccf1492c34
Pierre Genthon <genthon.pierre@gmail.com>
2024-03-18 16:45:09 +0100

    signup front pass validation & confirmation

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -2,16 +2,38 @@ import Layout from "@/components/Layout";
 import { useSignupMutation } from "@/graphql/generated/schema";
 import { FormEvent, useState } from "react";
 
-export default function NewAd() {
+function validatePassword(p: string) {
+  let errors = [];
+  if (p.length < 8)
+    errors.push("Le mot de passe doit faire minimum 8 caractères");
+  if (p.search(/[a-z]/) < 0)
+    errors.push("Le mot de passe doit contenir une minuscule");
+  if (p.search(/[A-Z]/) < 0)
+    errors.push("Le mot de passe doit contenir une majuscule");
+  if (p.search(/[0-9]/) < 0)
+    errors.push("Le mot de passe doit contenir un chiffre");
+
+  return errors;
+}
+
+export default function Signup() {
   const [error, setError] = useState("");
   const [createUser] = useSignupMutation();
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    setError("");
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const formJSON: any = Object.fromEntries(formData.entries());
 
-    setError("");
+    const errors = validatePassword(formJSON.password);
+    if (errors.length > 0) return setError(errors.join("\n"));
+    if (formJSON.password !== formJSON.passwordConfirmation)
+      return setError("les mots de passe ne coresspondent pas");
+
+    // do not send confirmation since it's checked on the frontend
+    delete formJSON.passwordConfirmation;
+
     try {
       const res = await createUser({ variables: { data: formJSON } });
       console.log({ res });
@@ -29,7 +51,7 @@ export default function NewAd() {
 
       <form onSubmit={handleSubmit} className="pb-12">
         <div className="flex flex-wrap gap-6 mb-3">
-          <div className="form-control w-full">
+          <div className="form-control w-full max-w-xs">
             <label className="label" htmlFor="email">
               <span className="label-text">Email</span>
             </label>
@@ -39,21 +61,6 @@ export default function NewAd() {
               name="email"
               id="email"
               autoComplete=""
-              className="input input-bordered w-full"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-wrap gap-6 mb-3">
-          <div className="form-control w-full max-w-xs">
-            <label className="label" htmlFor="password">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              required
               className="input input-bordered w-full max-w-xs"
             />
           </div>
@@ -74,7 +81,35 @@ export default function NewAd() {
           </div>
         </div>
 
-        {error !== "" && <div className="text-red-700">{error}</div>}
+        <div className="flex flex-wrap gap-6 mb-3">
+          <div className="form-control w-full max-w-xs">
+            <label className="label" htmlFor="password">
+              <span className="label-text">Mot de passe</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <div className="form-control w-full max-w-xs">
+            <label className="label" htmlFor="passwordConfirmation">
+              <span className="label-text">Confirmation</span>
+            </label>
+            <input
+              type="password"
+              name="passwordConfirmation"
+              id="passwordConfirmation"
+              required
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+        </div>
+
+        {error !== "" && <pre className="text-red-700">{error}</pre>}
         <button className="btn btn-primary text-white mt-12 w-full">
           Envoyer
         </button>
