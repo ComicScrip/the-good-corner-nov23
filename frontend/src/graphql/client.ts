8468ebffd3317965e6f22fbd4b08f67051975a57
Pierre Genthon <genthon.pierre@gmail.com>
2024-04-10 16:00:04 +0200

    add upload service docker prod

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const prodAPIUrl =
+  typeof window !== "undefined" ? `${window.location.origin}/graphql` : "";
+
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_API || undefined,
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_API || prodAPIUrl,
   cache: new InMemoryCache(),
   credentials: "include",
 });
