4247bba5c15546aa703efe2dc9873ccf125b7670
Pierre Genthon <genthon.pierre@gmail.com>
2024-04-29 17:37:49 +0200

    first component test

--- frontend/src/__tests__/AdCard.test.tsx
+++ frontend/src/__tests__/AdCard.test.tsx
@@ -1,7 +1,24 @@
 import { describe, it, expect } from "@jest/globals";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import AdCard from "../components/AdCard";
 
 describe("AdCard component", () => {
-  it("renders correctly", () => {});
+  it("renders correctly", () => {
+    const view = render(
+      <AdCard
+        ad={{
+          id: 1,
+          title: "Mon annonce",
+          price: 42,
+          picture: "https://img.com/img.png",
+        }}
+        link="http://localhost:3000/ads/1"
+      />
+    );
+
+    // expect(screen.getByText(/Mon annonce/)).toBeInTheDocument();
+    // expect(screen.getByText(/42/)).toBeInTheDocument();
+
+    expect(view.baseElement).toMatchSnapshot();
+  });
 });
