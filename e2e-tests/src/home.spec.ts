2ca47a7e755afd2d5d2471c73c77ce3259ac4e5f
Pierre Genthon <genthon.pierre@gmail.com>
2024-06-04 20:03:34 +0200

    e2e test failure

--- e2e-tests/src/home.spec.ts
+++ e2e-tests/src/home.spec.ts
@@ -56,7 +56,7 @@ test("can view ads in db", async ({ page }) => {
   await page.goto("/");
   await expect(
     page.getByRole("heading", {
-      name: "Annonces Rcentes",
+      name: "Annonces Récentes",
     })
   ).toBeVisible();
 
