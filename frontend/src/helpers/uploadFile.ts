c7b624f5d28450a1bc17444504ce15fc0a9098c1
Pierre <genthon.pierre@gmail.com>
2024-07-02 15:23:19 +0200

    Update uploadFile.ts

--- frontend/src/helpers/uploadFile.ts
+++ frontend/src/helpers/uploadFile.ts
@@ -4,7 +4,7 @@ export default async function uploadFile(file: File) {
   const data = new FormData();
   data.append("file", file);
   const res = await axios.post<{ url: string }>(
-    "http://localhost:8000/uploads",
+    "/uploads",
     data
   );
   return res.data.url;
