32319308462bf45cf4c617d9743f31ba41140da3
Pierre Genthon <genthon.pierre@gmail.com>
2024-07-09 00:34:00 +0200

    tmp

--- install_debian.sh
+++ install_debian.sh
@@ -9,14 +9,26 @@ WD=$(pwd)
 sudo apt-get update && \
 
 # Preapre docker install : https://docs.docker.com/engine/install/debian/
-sudo apt-get install ca-certificates curl && \
-sudo install -m 0755 -d /etc/apt/keyrings && \
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc && \
-sudo chmod a+r /etc/apt/keyrings/docker.asc && \
-echo \
- "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
- $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
- sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+sudo apt-get install ca-certificates curl;
+sudo install -m 0755 -d /etc/apt/keyrings;
+
+if lsb_release -a | grep Ubuntu; then # Ubuntu
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc;
+  sudo chmod a+r /etc/apt/keyrings/docker.asc;
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+  sudo apt-get update
+else # Debian
+  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc;
+  sudo chmod a+r /etc/apt/keyrings/docker.asc && \
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable";
+fi
+
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
 sudo apt-get update && \
 
 # Install Docker, Caddy, Go, Webhook, Fail2ban
