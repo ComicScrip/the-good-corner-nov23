805a0d9e679195f7047ba5bf87d60958b487adad
Pierre Genthon <genthon.pierre@gmail.com>
2024-07-25 13:38:36 +0200

    backup script

--- create_backup.sh
+++ create_backup.sh
@@ -8,13 +8,18 @@ CURRENT_BACKUP_FOLDER=$BACKUPS_FOLDER/`date +%Y-%m-%d"_"%H-%M-%S`
 RCLONE_REMOTE_NAME="idrive"
 RCLONE_REMOTE_FOLDER="tgc-backups"
 
+echo "syncing from remote to local..."
+
+rclone sync $RCLONE_REMOTE_NAME:$RCLONE_REMOTE_FOLDER $BACKUPS_FOLDER
+
+
 echo creating backup folder in $CURRENT_BACKUP_FOLDER
 
 mkdir -p $CURRENT_BACKUP_FOLDER
 docker exec $DB_CONTAINER_NAME pg_dumpall -c -U $DB_USERNAME > $CURRENT_BACKUP_FOLDER/dump.sql
 docker cp $UPLOADS_CONTAINER_NAME:/app/files $CURRENT_BACKUP_FOLDER
 
+echo "syncing from local to remote..."
 rclone sync $BACKUPS_FOLDER  $RCLONE_REMOTE_NAME:$RCLONE_REMOTE_FOLDER
 
 echo "done !"
-  
