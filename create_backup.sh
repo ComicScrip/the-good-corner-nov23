8e744ef4faf3f2a78bd9fa161d1f31571160dc35
Pierre Genthon <genthon.pierre@gmail.com>
2024-07-25 13:21:36 +0200

    backup script

--- create_backup.sh
+++ create_backup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-CONTAINER_NAME="prod-db-1"
+DB_CONTAINER_NAME="prod-db-1"
+UPLOADS_CONTAINER_NAME="prod-upload-service-1"
 DB_USERNAME="postgres"
 BACKUPS_FOLDER=".backups"
 CURRENT_BACKUP_FOLDER=$BACKUPS_FOLDER/`date +%Y-%m-%d"_"%H-%M-%S`
 RCLONE_REMOTE_NAME="idrive"
 RCLONE_REMOTE_FOLDER="tgc-backups"
 
-echo $CURRENT_BACKUP_FOLDER
+echo creating backup folder in $CURRENT_BACKUP_FOLDER
 
 mkdir -p $CURRENT_BACKUP_FOLDER
-docker exec $CONTAINER_NAME pg_dumpall -c -U $DB_USERNAME > $CURRENT_BACKUP_FOLDER/dump.sql
+docker exec $DB_CONTAINER_NAME pg_dumpall -c -U $DB_USERNAME > $CURRENT_BACKUP_FOLDER/dump.sql
+docker cp $UPLOADS_CONTAINER_NAME:/app/files $CURRENT_BACKUP_FOLDER
+
 rclone sync $BACKUPS_FOLDER  $RCLONE_REMOTE_NAME:$RCLONE_REMOTE_FOLDER
+
+echo "done !"
